2f72176f351ab68cde09c3c416a35be4337fb479
bidkar <bidkar.aragon@udo.mx>
2018-11-21 08:44:25 -0700

    stored procedure

--- Tercer Parcial/Funciones.sql
+++ Tercer Parcial/Funciones.sql
@@ -69,13 +69,14 @@ END;;
 
 -- definir una funcion que obtenga el porcentaje de asistencias por materia de un alumno
 DELIMITER ;;
+DROP FUNCTION IF EXISTS PorcentajeAsistenciaPorAlumnoAsignatura;;
 CREATE FUNCTION PorcentajeAsistenciaPorAlumnoAsignatura(id_grupo INT, id_asignatura INT, id_alumno INT)
 RETURNS INT
 BEGIN
     DECLARE resultado INT;
     SET resultado = 0;
 
-    SELECT TotalAsistenciaPorAlumnoAsignatura(grupo_id, asignatura_id, alumno_id)/MaxAsistenciaPorPeriodo(grupo_id, asignatura_id)
+    SELECT (TotalAsistenciaPorAlumnoAsignatura(grupo_id, asignatura_id, alumno_id)/MaxAsistenciaPorPeriodo(grupo_id, asignatura_id))*100
       FROM calificaciones
      WHERE grupo_id = id_grupo
        AND asignatura_id = id_asignatura
@@ -86,19 +87,57 @@ BEGIN
 END;;
 
 -- definir una consulta que obtenga el resultado de una boleta
-DELIMITER ;
-select
-	s.id as 'clave_asignatura',
-  s.nombre as 'asignatura',
-  c.parcial1,
-  c.parcial2,
-  c.parcial3,
-  c.asistencias1,
-  c.asistencias2,
-  c.asistencias3,
-  PorcentajeAsistenciaPorAlumnoAsignatura(c.grupo_id, c.asignatura_id, c.alumno_id) as 'porcentaje_asistencia'
-from calificaciones c
-join asignaturas s
-  on c.asignatura_id = s.id
-where grupo_id = 11
-  and alumno_id = 1;
+DELIMITER ;;
+CREATE PROCEDURE ImprimirBoletasGrupales(
+  IN _periodo VARCHAR(30),
+  IN _turno VARCHAR(10),
+  IN _semestre INT,
+  IN _carrera INT,
+  IN _grupo CHAR(6)
+)
+BEGIN
+  DECLARE id_grupo INT;
+  SET id_grupo = NULL;
+
+  SELECT id
+    FROM grupos
+   WHERE periodo = _periodo
+     AND turno = _turno
+     AND semestre = _semestre
+     AND carrera_id = _carrera
+     AND nombre = _grupo
+    INTO id_grupo;
+
+  select
+    g.id as 'grupo_id',
+    g.periodo,
+    g.turno,
+    g.semestre,
+    g.nombre as 'grupo',
+    r.nombre as 'carrera',
+    a.id as 'alumno_id',
+    concat(a.nombre,' ',a.paterno,' ',a.materno) as 'alumno',
+    a.no_control,
+    a.generacion,
+    s.id as 'clave_asignatura',
+    s.nombre as 'asignatura',
+    ifnull(c.parcial1,'') as 'parcial1',
+    ifnull(c.parcial2,'') as 'parcial2',
+    ifnull(c.parcial3,'') as 'parcial3',
+    ifnull(c.asistencias1,'') as 'asistencias1',
+    ifnull(c.asistencias2,'') as 'asistencias2',
+    ifnull(c.asistencias3,'') as 'asistencias3',
+    PorcentajeAsistenciaPorAlumnoAsignatura(c.grupo_id, c.asignatura_id, c.alumno_id) as 'porcentaje_asistencia'
+  from calificaciones c
+  join asignaturas s
+    on c.asignatura_id = s.id
+  join grupos g
+    on c.grupo_id = g.id
+  join carreras r
+    on g.carrera_id = r.id
+  join alumnos a
+    on c.alumno_id = a.id
+  where grupo_id = id_grupo;
+END;;
+
+CALL ImprimirBoletasGrupales('semestral 1 - 2018', 'vespertino', 3, 4, '3av-pr');;
